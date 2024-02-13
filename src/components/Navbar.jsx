a8a076b6a2a5a411e76eb778799840ce1b222a61
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2024-02-14 01:27:53 +0800

    Create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return (
+        <nav>
+            <img src="/logo.png" ></img>
+            <a>Shop</a>
+            <img src="/shopping-cart.png"></img>
+            <img src="/search.png"></img>
+
+
+        </nav>
+
+
+    )
+}
