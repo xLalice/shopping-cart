c3270cde5726ad949d61aad81a07bd6dd6e50e58
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2024-02-22 02:36:22 +0800

    Fix card design and layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.scss";
 export default function Navbar(){
     return (
         <nav>
-            <h1 className="logo">PeRfEcTfIt</h1>
+            <h1 className="logo"><Link to="/">PeRfEcTfIt</Link></h1>
             <Link to="shop">Shop</Link>
             <img className="icon" src="/search-interface-symbol.png"></img>
             <img className="icon" src="/shopping-cart(1).png"></img>
