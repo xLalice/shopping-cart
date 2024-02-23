fec80a055a1e6097bc7d16fef85e2db141efffbe
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2024-02-24 01:38:44 +0800

    Create Shop and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import "./App.scss"
+import { useState } from "react";
 
 function App() {
+	const [cart, setCart] = useState([]);
 
-  return (
-    <div>
-      <Navbar/>
-      <Home/>
-      <Shop/>
-    </div>
-  )
+	function addToCart(product){
+		setCart(prevCart => [...prevCart, product]);
+	}
+
+	return (
+	<div>
+		<Navbar/>
+		<Home/>
+		<Shop addToCart={addToCart} cart={cart} />
+	</div>
+	)
 }
 
 export default App
